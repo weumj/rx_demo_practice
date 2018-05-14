78fe9c96b1a6ee87b521f998874362a1edba5015
Myungjin Kim <weumj1@gmail.com>
2018-05-15 03:29:15 +0900

    chap 2. autocomplete - 2

--- demo/js/2.autocomplete.js
+++ demo/js/2.autocomplete.js
@@ -1,21 +1,45 @@
+const $loading = document.getElementById("loading");
+
+const showLoading = () => {
+  $loading.style.display = "block";
+};
+const hideLoading = () => {
+  $loading.style.display = "none";
+};
+
 const { fromEvent } = rxjs;
 const {
   map,
-  mergeMap,
   debounceTime,
-  filter,
+  partition,
+  tap,
   distinctUntilChanged,
+  switchMap,
+  catchError,
+  retry,
+  finalize,
 } = rxjs.operators;
 const { ajax } = rxjs.ajax;
-const user$ = fromEvent(document.getElementById("search"), "keyup").pipe(
-  debounceTime(300),
+
+const keyup$ = fromEvent(document.getElementById("search"), "keyup").pipe(
+  debounceTime(300), // 300ms 뒤에 데이터를 전달한다.
   map(event => event.target.value),
-  distinctUntilChanged(),
-  filter(query => query.trim().length > 0),
-  mergeMap(query =>
+  distinctUntilChanged(), // 특수키가 입력된 경우에는 나오지 않기 위해 중복 데이터 처리
+);
+
+let [user$, reset$] = keyup$.pipe(partition(query => query.trim().length > 0));
+
+user$ = user$.pipe(
+  tap(showLoading),
+  switchMap(query =>
     ajax.getJSON(`https://api.github.com/search/users?q=${query}`),
   ),
+  tap(hideLoading),
+  retry(2),
+  finalize(hideLoading),
 );
+
+reset$.pipe(tap(() => ($layer.innerHTML = ""))).subscribe();
 
 const $layer = document.getElementById("suggestLayer");
 
@@ -33,6 +57,12 @@ const drawLayer = items => {
     .join("");
 };
 
-user$.subscribe(v => {
-  drawLayer(v.items);
+user$.subscribe({
+  next: v => {
+    drawLayer(v.items);
+  },
+  error: e => {
+    console.error(e);
+    alert(e.message);
+  },
 });
