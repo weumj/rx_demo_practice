0a93f20c5efbc428b6db430bdc7fdd6c60ab0f0f
Myungjin Kim <weumj1@gmail.com>
2018-05-16 04:05:22 +0900

    chap 3. map demo - 3 - render autocomplete

--- demo/4.demo/autocomplete.js
+++ demo/4.demo/autocomplete.js
@@ -1,3 +1,84 @@
+const {
+  fromEvent,
+  operators: {
+    pluck,
+    finalize,
+    retry,
+    debounceTime,
+    map,
+    distinctUntilChanged,
+    share,
+    tap,
+    switchMap,
+    merge,
+    partition,
+  },
+  ajax: { ajax },
+} = rxjs;
+
 export default class AutoComplete {
-  constructor($autocomplete) {}
+  constructor($autocomplete) {
+    this.$input = $autocomplete.querySelector("input");
+    this.$layer = $autocomplete.querySelector(".layer");
+    this.$loading = $autocomplete.querySelector(".loading");
+
+    let [search$, reset$] = this.createKeyup$().pipe(
+      partition(query => query.trim().length > 0)
+    );
+
+    search$ = search$.pipe(
+      tap(() => this.showLoading()),
+      switchMap(query => ajax.getJSON(`/bus/${query}`)),
+      pluck("busRouteList"),
+      retry(2),
+      tap(() => this.hideLoading()),
+      finalize(() => {
+        this.hideLoading();
+        this.$layer.style.display = "none";
+      }),
+      finalize(() => this.reset())
+    );
+    reset$ = reset$.pipe(
+      merge(fromEvent(this.$layer, "click", evt => evt.target.closest("li")))
+    );
+
+    search$.subscribe(items => this.render(items));
+    reset$.subscribe(() => this.reset());
+  }
+
+  createKeyup$() {
+    return fromEvent(this.$input, "keyup").pipe(
+      debounceTime(300),
+      map(event => event.target.value),
+      distinctUntilChanged(),
+      share()
+    );
+  }
+
+  showLoading() {
+    this.$loading.style.display = "block";
+  }
+  hideLoading() {
+    this.$loading.style.display = "none";
+  }
+  reset() {
+    this.hideLoading();
+    this.$layer.style.display = "none";
+  }
+
+  render(buses) {
+    this.$layer.innerHTML = buses
+      .map(bus => {
+        return `
+        <li>
+          <a href="#">
+              <strong>${bus.routeName}</strong>
+              <span>${bus.regionName}</span>
+              <div>${bus.routeTypeName}</div>
+          </a>
+        </li>`;
+      })
+      .join("");
+    this.$layer.style.display = "block";
+  }
 }
